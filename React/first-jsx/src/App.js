56b2ce8c99bab9bb30696e0f50550b31c2f83cf6
Muath-Ademar <muazademar20@gmail.com>
2024-07-28 11:36:29 +0300

    First React with JSX

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!!</h1>
+      <h1>things I need to do:</h1>
+      <ul>
+      <li>Learn React</li>
+      <li>Climb Mt.Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+      </ul>
+       
+    </div>
+  )
+}
