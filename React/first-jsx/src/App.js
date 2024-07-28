81a9a54672e139bccd7b505223d4bd6cdbeb9836
Muath-Ademar <muazademar20@gmail.com>
2024-07-28 15:10:51 +0300

    Prop it Up

--- React/first-jsx/src/App.js
+++ React/first-jsx/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import PersonCard from './PersonCard'
 
 export default function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>Hello Dojo!!</h1>
       <h1>things I need to do:</h1>
       <ul>
@@ -11,7 +12,11 @@ export default function App() {
       <li>Run a marathon</li>
       <li>Feed the dogs</li>
       </ul>
-       
+    <PersonCard firstName = {"Muath"} lastName = {"Ez Zughayyar"} age = {"22"} hairColor = {"Brown"}/>
+    <PersonCard firstName = {"Omar"} lastName = {"Ez Zughayyar"} age = {"35"} hairColor = {"Black"}/>
+    <PersonCard firstName = {"Mohammed"} lastName = {"Ez Zughayyar"} age = {"28"} hairColor = {"Black"}/>
+    <PersonCard firstName = {"Ademar"} lastName = {"Ez Zughayyar"} age = {"71"} hairColor = {"white"}/>
     </div>
+
   )
 }
---
+++ React/first-jsx/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = (props)=>{
+return (
+    <div>
+    <h1>My name is {props.firstName} {props.lastName}</h1>
+    </div>
+)
+}
+export default Header
