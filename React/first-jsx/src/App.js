e145b3bbc43b304731a1857dafd0dff3b731202f
Muath-Ademar <muazademar20@gmail.com>
2024-07-29 17:14:41 +0300

    Putting it all toghther

--- React/first-jsx/src/App.js
+++ React/first-jsx/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PersonCard from './PersonCard'
 
+
+
+
 export default function App() {
   return (
     <div className='App'>
@@ -12,10 +15,13 @@ export default function App() {
       <li>Run a marathon</li>
       <li>Feed the dogs</li>
       </ul>
-    <PersonCard firstName = {"Muath"} lastName = {"Ez Zughayyar"} age = {"22"} hairColor = {"Brown"}/>
-    <PersonCard firstName = {"Omar"} lastName = {"Ez Zughayyar"} age = {"35"} hairColor = {"Black"}/>
-    <PersonCard firstName = {"Mohammed"} lastName = {"Ez Zughayyar"} age = {"28"} hairColor = {"Black"}/>
-    <PersonCard firstName = {"Ademar"} lastName = {"Ez Zughayyar"} age = {"71"} hairColor = {"white"}/>
+    <PersonCard  firstName = {"Muath"} lastName = {"Ez Zughayyar"} age = {22} hairColor = {"Brown"}/>
+    <PersonCard firstName = {"Omar"} lastName = {"Ez Zughayyar"} age = {35} hairColor = {"Black"}/>
+    <PersonCard firstName = {"Mohammed"} lastName = {"Ez Zughayyar"} age = {28} hairColor = {"Black"}/>
+    <PersonCard firstName = {"Ademar"} lastName = {"Ez Zughayyar"} age = {71} hairColor = {"white"}/>
+    
+    
+    
     </div>
 
   )
--- React/first-jsx/src/PersonCard.js
+++ React/first-jsx/src/PersonCard.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from "react"
+
+
 
 
 
     const PersonCard = (props)=> {
+      const [age, setAge] = useState(props.age);
+      const increaseAge = ()=>{
+        setAge(
+           age + 1
+
+        );
+      }
+
   return (
     <div>
     <h1>{props.firstName}, {props.lastName}</h1>
-    <p>Age: {props.age}:</p>
+    <p>Age: {age}</p>
     <p>Hair: {props.hairColor}</p>
+    <button onClick={increaseAge}>Birthday button for {props.firstName}, {props.lastName}</button>
+    
+
 
     </div>
   )
